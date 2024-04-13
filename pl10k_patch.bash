6f779012ecad08a2143ff0c79bc0397ef0d2c121
henintsoa98 <38418989+henintsoa98@users.noreply.github.com>
2024-04-13 11:43:47 +0300

    Add powerlevel10k patch to show battery, user@host

---
+++ pl10k_patch.bash
@@ -0,0 +1,7 @@
+# Author  : ONJANIAINA Henintsoa Stephana
+# Created : 13/04/24 11:40:57
+# Repo : henintsoa98/dotfiles.debian12
+# File : pl10k_patch.bash
+
+sed -i "s/# battery/battery/" ~/.p10k.zsh
+sed -i "s%typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}%# typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}%" .p10k.zsh
